56d99af1c73cc6fe42657a7d4e7a7288e8561f32
Yorrick Jansen <yorrick.jansen@gmail.com>
2014-08-16 17:11:25 -0400

    pretty print

--- src/main/scala/TwitterStream.scala
+++ src/main/scala/TwitterStream.scala
@@ -1,5 +1,6 @@
 import java.io.File
 import com.typesafe.config.ConfigFactory
+import org.apache.spark.rdd.RDD
 
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import StreamingContext._
@@ -19,10 +20,12 @@ object TwitterStream {
     System.setProperty("twitter4j.oauth.accessTokenSecret", conf.getString("accessTokenSecret"))
   }
 
+  val interval = Seconds(5)
+  val window = Seconds(30)
+
   def main(args: Array[String]) {
     configureTwitterOAuth()
 
-    val interval = Seconds(5)
 
     val ssc = new StreamingContext("local[4]", "TwitterStream", interval)
     val stream = TwitterUtils.createStream(ssc, None, args)
@@ -30,11 +33,16 @@ object TwitterStream {
     val hashTags: DStream[String] = stream.flatMap(status => status.getText.split(" ").filter(_.startsWith("#")))
 
     val pairs: DStream[(String, Int)] = hashTags.map(tag => (tag, 1))
-    val countByHashTag: DStream[(String, Int)] = pairs .reduceByKeyAndWindow(_ + _, Seconds(60))
+    val sum = (c1: Int, c2: Int) => c1 + c2
+    val countByHashTag: DStream[(String, Int)] = pairs reduceByKeyAndWindow(sum, window)
     val hashTagByCount: DStream[(Int, String)] = countByHashTag map {case (tag, count) => (count, tag)}
-    val topCounts60 = hashTagByCount.transform(_.sortByKey(false))
+    val topCounts60: DStream[(Int, String)] = hashTagByCount.transform(_.sortByKey(false))
 
-    topCounts60.print()
+    topCounts60 foreachRDD { rdd: RDD[(Int, String)] =>
+      val topList: Seq[(Int, String)] = rdd.take(10)
+      println(s"\nPopular topics in last ${window.milliseconds / 1000} seconds (${rdd.count}) total):")
+      topList foreach { case (count, tag) => println(s"$count: $tag")}
+    }
 
     ssc.start()             // Start the computation
     ssc.awaitTermination()  // Wait for the computation to terminate
